f8991fd81668c67081dc6c82e8a32043f916f7c3
karn21 <karn212000@gmail.com>
2020-03-06 17:25:19 +0530

    initialised files

---
+++ basic/settings/production.py
@@ -0,0 +1,13 @@
+from .base import *
+
+DEBUG = config('BEBUG', cast=bool)
+
+ALLOWED_HOSTS = ['ip-address','www.site.com']
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
+]
---
+++ basic/urls.py
@@ -0,0 +1,16 @@
+
+from django.contrib import admin
+from django.urls import path,include
+from django.conf import settings
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home ,name='home'),
+]
+
+if settings.DEBUG:
+	import debug_toolbar
+	urlpatterns = [path('__debug__/', include(debug_toolbar.urls)),
+
+	] + urlpatterns
